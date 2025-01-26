37cd67d9c5371ad0d4ca81bc0a9a37d469a145b8
Bestian Tang <bestian@gmail.com>
2025-01-26 11:17:30 +0800

    build

---
+++ dist/pwa/assets/use-timeout-DxgT0Vfz.js
@@ -0,0 +1,1 @@
+import{v as u}from"./vm-lekDuWU2.js";import{a3 as i,b as m,g as s}from"./index-DFiGA5Zc.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
