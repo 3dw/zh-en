fd4d58595633396170c0029a37081182fba4ab8c
Bestian Tang <bestian@gmail.com>
2025-03-17 17:40:53 +0800

    build again

---
+++ dist/spa/assets/use-quasar-CSTJ2dq9.js
@@ -0,0 +1,1 @@
+import{e as a,bf as r}from"./index-DDo0DLed.js";function s(){return a(r)}export{s as u};
--- dist/spa/assets/use-quasar-fxDOj-Da.js
+++
@@ -1,1 +0,0 @@
-import{e as a,be as e}from"./index-BXyAIsG6.js";function s(){return a(e)}export{s as u};
