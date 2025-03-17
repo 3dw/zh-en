eccb86485c298b4e12c7933d3e44b6e6d6125802
Bestian Tang <bestian@gmail.com>
2025-03-17 17:16:43 +0800

    build 2025-03-17, 終於成功了 close #49

--- dist/spa/assets/use-quasar-96ZBGOZf.js
+++
@@ -1,1 +0,0 @@
-import{e as a,be as e}from"./index-u7gP2akL.js";function s(){return a(e)}export{s as u};
---
+++ dist/spa/assets/use-quasar-fxDOj-Da.js
@@ -0,0 +1,1 @@
+import{e as a,be as e}from"./index-BXyAIsG6.js";function s(){return a(e)}export{s as u};
