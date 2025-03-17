a9e86017fe9bebd55ebecbdd2827f78fbf869d62
Bestian Tang <bestian@gmail.com>
2025-03-17 14:49:41 +0800

    build for debug

--- dist/spa/assets/use-quasar-CoM1Ff22.js
+++
@@ -1,1 +0,0 @@
-import{e as a,be as e}from"./index-DxbHAXU6.js";function s(){return a(e)}export{s as u};
---
+++ dist/spa/assets/use-quasar-FJUGCLDY.js
@@ -0,0 +1,1 @@
+import{e as a,be as e}from"./index-BBCvlH2p.js";function s(){return a(e)}export{s as u};
