81b838b44cca737826c4d29929a6edeaeaf703c0
Bestian Tang <109048+bestian@users.noreply.github.com>
2025-08-06 09:49:15 +0800

    rebuild

--- dist/spa/assets/use-quasar-B8U9csu4.js
+++
@@ -1,1 +0,0 @@
-import{e as a,bf as r}from"./index-CRVyQ5Pk.js";function s(){return a(r)}export{s as u};
---
+++ dist/spa/assets/use-quasar-m38pXLxa.js
@@ -0,0 +1,1 @@
+import{e as a,bf as r}from"./index-B2hqN4kE.js";function s(){return a(r)}export{s as u};
