3c8b5cab5cfbd1cd8fcb6826d5eae8a40cdf6914
Bestian Tang <109048+bestian@users.noreply.github.com>
2026-04-13 16:13:58 +0800

    build again

--- dist/spa/assets/use-quasar-CszYn3x-.js
+++
@@ -1,1 +0,0 @@
-import{e as a,bf as r}from"./index-DgS7g8DX.js";function s(){return a(r)}export{s as u};
---
+++ dist/spa/assets/use-quasar-DnsgO9yD.js
@@ -0,0 +1,1 @@
+import{e as a,bf as r}from"./index-Cn5z05FH.js";function s(){return a(r)}export{s as u};
